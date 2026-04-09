8eba1b981ee4a3a2b595af8075a624eaee404b95
Shaquille Kiragu <skiragu77@gmail.com>
2026-04-09 10:16:22 +0100

    init frontend dir with nuxt and tailwind

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+	<main class="w-screen min-h-screen flex justify-center mt-25">
+		<h1 class="text-4xl font-extrabold">Hello World!</h1>
+	</main>
+</template>
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ]
+    }
+  },
+})
