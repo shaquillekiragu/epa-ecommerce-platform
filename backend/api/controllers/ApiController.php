7f5d23ae869ddd540913529c5041775d96763bf8
Shaquille Kiragu <skiragu77@gmail.com>
2026-04-16 13:44:10 +0100

    api comments and boilerplate

---
+++ backend/api/controllers/ApiController.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace api\controllers;
+
+class ApiController {
+}
--- backend/api/models/Address.php
+++ backend/api/models/Address.php
@@ -6,3 +6,5 @@ use common\models\Address as CommonAddress;
 
 class Address extends CommonAddress {
 }
+
+// study the difference between the common\model, admin\model, customer\model, and api\model for the FAQ in your portfolio...
