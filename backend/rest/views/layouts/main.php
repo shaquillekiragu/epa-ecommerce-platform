703c2306fa365a483a91544a1ed09a00b6b26371
Shaquille Kiragu <skiragu77@gmail.com>
2026-04-16 15:20:36 +0100

    fix incorrect tailwind utility usage

--- backend/rest/views/layouts/main.php
+++ backend/rest/views/layouts/main.php
@@ -58,7 +58,7 @@ NavBar::end();
 ?>
 </header>
 
-<main role="main" class="flex-shrink-0">
+<main role="main" class="shrink-0">
     <div class="container">
         <?= Breadcrumbs::widget([
         'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
