a396c527945f8be79b11037c25375d540a0415a9
Shaquille Kiragu <skiragu77@gmail.com>
2026-04-21 19:48:00 +0100

    refactor adminpanelcontroller and build admin-panel index view file

--- backend/superadmin/controllers/AdminPanelController.php
+++ backend/superadmin/controllers/AdminPanelController.php
@@ -2,34 +2,32 @@
 
 namespace superadmin\controllers;
 
+use superadmin\models\User;
+
 class AdminPanelController extends _SuperadminWebController
 {
     /** @var class-string<\yii\db\ActiveRecord> */
-    public string $model_class;
+    public $model_class;
 
     /** @var class-string<\superadmin\models\search\_BaseSearch> */
-    public string $search_model_class;
+    public $search_model_class;
 
 	public function actionIndex()
     {
-        // $model_class = $this->model_class;
+        $model_class = $this->model_class ?? User::class;
 
-        // $data_provider = new \yii\data\ActiveDataProvider([
-        //     'query' => $model_class::find(),
-        //     'pagination' => ['pageSize' => 50],
-        // ]);
+        $data_provider = new \yii\data\ActiveDataProvider([
+            'query' => $model_class::find(),
+            'pagination' => ['pageSize' => 50],
+        ]);
 
-        // return $this->render('@superadmin/views/crud/index', [
-        //     'data_provider' => $data_provider,
-        //     'model_class' => $model_class,
+        return $this->render('@superadmin/views/admin-panel/index', [
+            'data_provider' => $data_provider,
+            'model_class' => $model_class,
 
-        // ]);
+        ]);
 
-        // return $this->render('', [
-
-        // ]);
-
-        return 'Hello world!';
+        // return 'Hello world!';
     }
 
     public function actionView($id)
