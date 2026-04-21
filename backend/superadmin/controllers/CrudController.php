28070a3d8e8bccab3d2186b11449e4349cdd1e45
Shaquille Kiragu <skiragu77@gmail.com>
2026-04-21 15:43:05 +0100

    create crudcontroller extending _superadminwebcontroller to contain base crud action functions

---
+++ backend/superadmin/controllers/CrudController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace superadmin\controllers;
+
+class CrudController extends _SuperadminWebController
+{
+    /** @var class-string<\yii\db\ActiveRecord> */
+    public string $model_class;
+
+    /** @var class-string<\superadmin\models\search\_BaseSearch> */
+    public string $search_model_class;
+
+	public function actionIndex()
+    {
+
+    }
+
+    public function actionView($id)
+    {
+        
+    }
+
+    public function actionCreate()
+    {
+        
+    }
+
+    public function actionUpdate($id)
+    {
+        
+    }
+
+    public function actionDelete($id)
+    {
+        
+    }
+}
