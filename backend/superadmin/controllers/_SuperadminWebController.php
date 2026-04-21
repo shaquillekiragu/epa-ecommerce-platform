8e1c8ffe2b45adee00713d9a34269f6950f672a3
Shaquille Kiragu <skiragu77@gmail.com>
2026-04-21 15:41:15 +0100

    switch from rest to yii web functionality in superadmin module

---
+++ backend/superadmin/controllers/_SuperadminWebController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace superadmin\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\ForbiddenHttpException;
+
+class _SuperadminWebController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'denyCallback' => [$this, 'handleUnauthorisedUser'],
+                'rules' => [
+                    // [
+                    //     'actions' => [
+                    //         'index',
+                    //         'view',
+                    //         'create',
+                    //         'update',
+                    //         'delete',
+                    //     ],
+                    //     'allow' => true,
+                    //     'roles' => ['superadmin'],
+                    // ],
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                        'matchCallback' => fn () => Yii::$app->user->can('superadmin'),
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function handleUnauthorisedUser()
+    {
+        if (Yii::$app->user->isGuest) {
+            return Yii::$app->response->redirect(['site/login']);
+        }
+        throw new ForbiddenHttpException('Superadmin access required.');
+    }
+}
+
+// research yii's in-built role functionality - auth item, assignment - allows you to define roles - ranked roles
