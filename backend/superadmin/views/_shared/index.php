c889477f4fd90b680da61f650da9795fca01b8a8
Shaquille Kiragu <skiragu77@gmail.com>
2026-04-25 21:05:08 +0100

    update dashboard index view

--- backend/superadmin/views/_shared/index.php
+++ backend/superadmin/views/_shared/index.php
@@ -1,20 +1,28 @@
 <?php
 
 use yii\grid\GridView;
-use yii\grid\SerialColumn;
 use yii\grid\ActionColumn;
-
-/** @var \yii\data\ActiveDataProvider $data_provider */
-/** @var class-string<\yii\db\ActiveRecord> $model_class */
 
 $model = new $model_class();
 
-echo GridView::widget([
-    'dataProvider' => $data_provider,
-    'columns' => array_merge(
-        $model->attributes(), // shows all DB columns
-        [['class' => ActionColumn::class]]
-    ),
-]);
-
 ?>
+
+<main class="my-5 mr-5">
+    <?= GridView::widget([
+        'dataProvider' => $data_provider,
+
+        'tableOptions' => ['class' => 'table table-striped table-bordered table-hover'],
+
+        'rowOptions' => function ($model) {
+            return [
+                'style' => 'cursor:pointer',
+                'onclick' => "window.location.href='" . \yii\helpers\Url::to(['view', 'id' => $model->id]) . "'",
+            ];
+        },
+
+        'columns' => array_merge(
+            $model->attributes(),
+            [['class' => ActionColumn::class]]
+        ),
+    ]); ?>
+</main>
