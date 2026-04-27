622dc48217735abba687c0424316a6e2c75f659f
Shaquille Kiragu <skiragu77@gmail.com>
2026-04-27 12:58:59 +0100

    build crud buttons in index and view views

--- backend/superadmin/views/_shared/index.php
+++ backend/superadmin/views/_shared/index.php
@@ -2,27 +2,64 @@
 
 use yii\grid\GridView;
 use yii\grid\ActionColumn;
+use yii\helpers\Html;
+use yii\helpers\Url;
 
 $model = new $model_class();
 
 ?>
 
-<main class="my-5 mr-5">
-    <?= GridView::widget([
-        'dataProvider' => $data_provider,
+<main class="my-5 pe-4">
+    <div class="table-responsive">
+        <?= GridView::widget([
+            'dataProvider' => $data_provider,
 
-        'tableOptions' => ['class' => 'table table-striped table-bordered table-hover'],
+            'tableOptions' => ['class' => 'table table-striped table-bordered table-hover'],
 
-        'rowOptions' => function ($model) {
-            return [
-                'style' => 'cursor:pointer',
-                'onclick' => "window.location.href='" . \yii\helpers\Url::to(['view', 'id' => $model->id]) . "'",
-            ];
-        },
+            'rowOptions' => function ($model) {
+                return [
+                    'style' => 'cursor:pointer',
+                    'onclick' => "window.location.href='" . Url::to(['view', 'id' => $model->id]) . "'",
+                ];
+            },
 
-        'columns' => array_merge(
-            $model->attributes(),
-            [['class' => ActionColumn::class]]
-        ),
-    ]); ?>
+            'columns' => array_merge(
+                $model->attributes(),
+                [[
+                    'class' => ActionColumn::class,
+                    'template' => '{view} {update} {delete}',
+
+                    'urlCreator' => function ($action, $model) {
+                        $pk = $model->getPrimaryKey();
+                        return Url::to([$action, 'id' => $pk]);
+                    },
+
+                    'contentOptions' => ['class' => 'd-flex gap-2'],
+                    'buttonOptions' => ['class' => ''],
+
+                    'buttons' => [
+                        'delete' => function ($url, $model, $key) {
+                            return Html::a(
+                                'Delete',
+                                $url,
+                                [
+                                    'class' => 'btn btn-sm btn-outline-danger',
+                                    'data' => [
+                                        'confirm' => 'Are you sure you want to delete this record?',
+                                        'method' => 'post',
+                                    ],
+                                ]
+                            );
+                        },
+                        'view' => function ($url) {
+                            return Html::a('View', $url, ['class' => 'btn btn-sm btn-outline-primary']);
+                        },
+                        'update' => function ($url) {
+                            return Html::a('Update', $url, ['class' => 'btn btn-sm btn-outline-warning']);
+                        },
+                    ],
+                ]],
+            ),
+        ]); ?>
+    </div>
 </main>
--- backend/superadmin/views/_shared/view.php
+++ backend/superadmin/views/_shared/view.php
@@ -1,12 +1,35 @@
 <?php
 
 use yii\widgets\DetailView;
+use yii\helpers\Html;
 
 ?>
 
-<main class="my-5">
+<main class="d-flex flex-column gap-5 my-5">
 	<?= DetailView::widget([
 		'model' => $model,
 		'attributes' => $model->attributes(),
 	]); ?>
+
+	<?php $pk = $model->getPrimaryKey(); ?>
+
+	<section class="d-flex justify-content-between">
+		<?= Html::a(
+			'Update',
+			['update', 'id' => $pk],
+			['class' => 'btn btn-outline-warning']
+		); ?>
+
+		<?= Html::a(
+			'Delete',
+			['delete', 'id' => $pk],
+			[
+				'class' => 'btn btn-outline-danger',
+				'data' => [
+					'confirm' => 'Are you sure you want to delete this record?',
+					'method' => 'post',
+				],
+			]
+		); ?>
+	</section>
 </main>
