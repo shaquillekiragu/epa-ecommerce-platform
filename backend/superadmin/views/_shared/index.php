f0085189a6208b355835b50e9d5cd015aa71b85b
Shaquille Kiragu <skiragu77@gmail.com>
2026-04-28 14:43:40 +0100

    remove broken delete button in index view table

--- backend/superadmin/views/_shared/index.php
+++ backend/superadmin/views/_shared/index.php
@@ -32,7 +32,7 @@ foreach ($columns as $i => $column) {
 
 $action_column = [[
     'class' => ActionColumn::class,
-    'template' => '{view} {update} {delete}',
+    'template' => '{view} {update}',
 
     'urlCreator' => function ($action, $model) {
         $pk = $model->getPrimaryKey();
@@ -43,24 +43,29 @@ $action_column = [[
     'buttonOptions' => ['class' => ''],
 
     'buttons' => [
-        'delete' => function ($url, $model, $key) {
-            return Html::a(
-                'Delete',
-                $url,
-                [
-                    'class' => 'btn btn-sm btn-outline-danger',
-                    'data' => [
-                        'confirm' => 'Are you sure you want to delete this record?',
-                        'method' => 'post',
-                    ],
-                ]
-            );
-        },
+        // 'delete' => function ($url, $model, $key) {
+        //     return Html::a(
+        //         'Delete',
+        //         $url,
+        //         [
+        //             'class' => 'btn btn-sm btn-outline-danger',
+        //             'onclick' => "if (typeof event !== 'undefined' && event) { event.stopPropagation(); }",
+        //             'data' => [
+        //                 'confirm' => 'Are you sure you want to delete this record?',
+        //                 'method' => 'post',
+        //             ],
+        //         ]
+        //     );
+        // },
         'view' => function ($url) {
-            return Html::a('View', $url, ['class' => 'btn btn-sm btn-outline-primary']);
+            return Html::a('View', $url, [
+                'class' => 'btn btn-sm btn-outline-primary',
+            ]);
         },
         'update' => function ($url) {
-            return Html::a('Update', $url, ['class' => 'btn btn-sm btn-outline-warning']);
+            return Html::a('Update', $url, [
+                'class' => 'btn btn-sm btn-outline-warning',
+            ]);
         },
     ],
 ]];
