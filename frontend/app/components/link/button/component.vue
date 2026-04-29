a55c444c921b4208775a19eb3b386f8d13f8d6be
Shaquille Kiragu <skiragu77@gmail.com>
2026-04-29 16:39:44 +0100

    create and use link button component

---
+++ frontend/app/components/link/button/component.vue
@@ -0,0 +1,20 @@
+<template>
+    <a :href="url" class="border px-6 py-3 transition-colors" :class="is_dark === true ? 'bg-black text-white' : 'bg-transparent text-black'">{{ text }}</a>
+</template>
+
+<script setup lang="ts">
+const { text, url, is_dark } = defineProps({
+    text: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    is_dark: {
+        type: Boolean,
+        required: true
+    },
+})
+</script>
