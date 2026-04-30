be474d940569729d18b58b932b2ceae8c3a50158
Shaquille Kiragu <skiragu77@gmail.com>
2026-04-30 13:13:20 +0100

    define product types

--- frontend/app/types/product.ts
+++ frontend/app/types/product.ts
@@ -1,9 +1,17 @@
-export interface Product {
-
-}
-
 export interface ProductCard {
     id: number;
     name: string;
     thumbnail: string;
+}
+
+export interface Product extends ProductCard {
+    store_id: number;
+    product_category_name: string;
+    description: string;
+    price_in_gbp: number;
+    number_in_stock: number;
+    sku_code: string;
+    weight_in_grams: number;
+    seo_title: string;
+    is_live: boolean;
 }
