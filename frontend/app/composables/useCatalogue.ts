25ae52cf1cc1a8aa17e66dde117a5cf4c6d66950
Shaquille Kiragu <skiragu77@gmail.com>
2026-04-30 16:27:10 +0100

    move product card data array into composable function

---
+++ frontend/app/composables/useCatalogue.ts
@@ -0,0 +1,98 @@
+import type { Product, ProductCard } from "~/types/product";
+
+// export function fetchProducts(): Product[] {
+// }
+
+export function getProductCards(): ProductCard[] {
+    return [
+        {
+            id: 1,
+            name: 'Series X Chronograph Smartwatch',
+            product_category_name: 'Electronics',
+            price_in_gbp: 499,
+            thumbnail: 'https://lh3.googleusercontent.com/aida-public/AB6AXuDfH9wPMTMr_wD6yLx_pbGR6aMEcyawkhlpBGqHTlqE7dfxFrDibbI3hWtqLMRNQb70Tgqh33_-ROtzqWhLXr95jGm021_GcKRxhX6NLTGbhAieM-0eKwFsAZMoaYNCgmRMshclnPnz7cPPOA7kXut6O_gvBQiv_4_rUlGtRWBMtptWuVX88bgb1K2cyRZPw2TD3r_AR3lLOAGBavsZ6EFLnhHtBlyKBo3gCjkduy2SmnbWFOgLpQpvysNuOUxrIdzk4veMMC2oWfPs',
+            product_url: ''
+        },
+        {
+            id: 2,
+            name: 'Artisan Leather Tote',
+            product_category_name: 'Fashion',
+            price_in_gbp: 285,
+            thumbnail:
+                'https://lh3.googleusercontent.com/aida-public/AB6AXuDAo9Zgpjz_whssbG4qQcGf5T85N0DH5FzylFdl1fPE_lN33BSswwMU0zIBuxe8tpH84kEj3etgBVkhOsfVRlHKzg6tcn_6Pd4UJ8mDG8ppijFEEP_vEKnlJhniZv0RcXZzk4-0W8IKhofhN5D0vWhw0eOcHo7005biU8aORsJPTLze12r0F_CH2eVQKmHdEePg4aIYzXefT4zhtj3LYjUyOvhn1f1NnaQ7UpFyupqa5xOvkLrHBKdXK6w1k7e-gDo5kXq1Vx617dgZ',
+            product_url: '',
+        },
+        {
+            id: 3,
+            name: 'Aura Pro Noise-Cancelling Audio',
+            product_category_name: 'Electronics',
+            price_in_gbp: 349,
+            thumbnail:
+                'https://lh3.googleusercontent.com/aida-public/AB6AXuDQ8j7F5WmQ1NqAfnkqK_2mxo6km14ozRgXsgjrSjfYpsKHY68xoF-ga7ZfSjuUDSaZ7VTzsBEQq3SJ32eh_zw6Zncozn68FcbyCenan0LWG2URk07fHOevaUx0vHwRuCQN80VHboALXrFcuAl0dkltyxNE0M2MHd7vswGQhOhZY4NfWAYZafq7qmxgcd0MDzn1OjDWm8zUdPIWGgZmdkeFXDlDFyTUV0JQZyvmnYgaJeQYckiCkEpX-ko7V9Xx_yRcGeDjX34cnK5v',
+            product_url: '',
+        },
+        {
+            id: 4,
+            name: 'Sculptural Ceramic Vase',
+            product_category_name: 'Home Decor',
+            price_in_gbp: 120,
+            thumbnail:
+                'https://lh3.googleusercontent.com/aida-public/AB6AXuCWXKImU18UrDzp-uX3wf3_EL77Ub7FUZ27zkJp-RHc5A9dsSbv4377VF6lpmjdrvjXW9PZkQdiX9wv02xC_-tdi3jXcy1VuwnoVGFrgRC31OnL4mg5jGrReSDiGl839-KoxZfbTpOXWtO8HcaLA1qXE6gVtJlyCi1554Q6HGH_HpJAnvhzeJG3iqaHgTgyfXb1CuTIBGQJsKA93Qu3YJSbHKTHsHgnX2eTry5RLyh6oEEndmYC_0a--t-YD10CSbW2XsY0uSr0tYSG',
+            product_url: '',
+        },
+        {
+            id: 5,
+            name: 'Chronograph Smartwatch',
+            product_category_name: '',
+            price_in_gbp: 299,
+            thumbnail:
+                'https://lh3.googleusercontent.com/aida-public/AB6AXuDQyyzuyPMo8Bju05LvbZVbO3ZfqLtfx673f6HTYbr1SSbDCQOHgoj_JFIJwNgl5MIEUCNR9qpDMm1VXiB4uhHtDE_rPR__NdAYvVqXiiDwUtYPzNQ17rqyZkfNfw2cTk4Ee4iJJjjFNLY04fI-uC83_lw1-cZZNSrIhpuchzdTrvw1eKKG4FQcIGVA9fxf-U2UczdkmBvWI11uBvR1IvhzHFUmlzvW0rR1c-zEh-e7GltOUEt_UhhgKQ2jePH3IagEp2vLl0zGyl-r',
+            product_url: '',
+        },
+        {
+            id: 6,
+            name: 'Aura Studio Headphones',
+            product_category_name: '',
+            price_in_gbp: 349.5,
+            thumbnail:
+                'https://lh3.googleusercontent.com/aida-public/AB6AXuB6g4Q5jZ-O9UP18dFdVUXlg-oygmNWEbD-jgvJyPF0m447xLG0A7iTz0IjpbqfhJyqms55JGxOovd60eKKfH2oD3zDzPA6TbcnvH9V_TH0R7Rkk35DrEZ3SC7NX48s0NLdAiUKnSMcXsfgJ0W7cVh0Wbjmi1WDaWX_t5nI4o7JXvzs7Wz9ye3744PGBq39E9-8sjHNGjVqGjeAg7-stwv-G1J_vbs2f-JA8kTls1YgkAFinIanqD510RejJpMyXWchpbBAjYn1OLG7',
+            product_url: '',
+        },
+        {
+            id: 7,
+            name: 'Artisan Pour-Over Set',
+            product_category_name: '',
+            price_in_gbp: 85,
+            thumbnail:
+                'https://lh3.googleusercontent.com/aida-public/AB6AXuBzXGnXhpG9OzkZmpZz-B73aGTXl1iNy1fKSLWuKaqWRCRvbPtzKORyRSNQf9ayl93z_wosLPt1qg7kXzTsSCgDXINK8mZLbXfe8IccIRbkRe_tX5NPEQA-_8f-F0NOVFrqUi3QLG2j5A1vtPtkwW_fJa5u1nt95zkI8ha-hHMnF7vfPzuez2bh9aKytMTqwJDXYAMcsVW_IqRm-MfMTFnk1rtfFGxNOVJRqg-i-SfQ_-wl952-4-p5qRbncJf6vVtVH3-KmQOvhNa5',
+            product_url: '',
+        },
+        {
+            id: 8,
+            name: 'Retro Instant Camera',
+            product_category_name: '',
+            price_in_gbp: 129.99,
+            thumbnail:
+                'https://lh3.googleusercontent.com/aida-public/AB6AXuA50OmyAKcg-1jOCo-NetkshDh_mmriA1RsStiBj1IJALJI86RY3Vm142_0LQByng0lGPfuho7GAMvfFLKfHTr5u-fFyz0swvy_4iDyMkNEjCAEytHD5gzWc4lXgVrWcNirVuPggYxfs7YVSawNtCXJtiUnURQnzJIxoj2b7EjiJLhCHpcKJZ4IayKSg-uol6vFeyYxi3zJWhIvuyI0WI5YumNtEM6xZ_vYBV5NeKzlSXOvfZq8n_Sj0IsYXoWv_lSu0UBn5D2sz3Tf',
+            product_url: '',
+        },
+        {
+            id: 9,
+            name: 'Velocity Runners',
+            product_category_name: '',
+            price_in_gbp: 165,
+            thumbnail:
+                'https://lh3.googleusercontent.com/aida-public/AB6AXuBanppMpZkWX00vOfTnPdov10Llx78bqqHkOuzZSrSLX-2pwl-W-_kT2xL88fkEy6G6Xl6kdTCqpH0YZjAuMNGWeiVFHEqu9VmiNV6WGGz6QxcPVl4C5wMhmE17e6cHkJkT3gwCYZ5F2FqgKZfTwWIt4N6FU0mlIsPoMp-OLtAdzuQqPZdjAMoC8sVX4a1xMvWRCsm5u3HSe8yHFecqYCUhNcHNJ86RbjrnJoh2iXQJXhKDbO6QTTJktKwUHYvg95VEDreJxKbgwHUN',
+            product_url: '',
+        },
+        {
+            id: 10,
+            name: 'Echo Home Hub',
+            product_category_name: '',
+            price_in_gbp: 199,
+            thumbnail:
+                'https://lh3.googleusercontent.com/aida-public/AB6AXuBTefx-Zr7C3Zz09rxeBvmAXBZicGaXOe12Q22BL8OySXqwHFQsktmta50p3IZaQrHDzSZSrNfXJVygilydEZb_uHpKGWSYCqugICYGGaFaX2GQV_v-HY3DIax-dVRdWJqToCtDcg-_fYu2HyJzdIS_Q035sRN-tpS0ComlgmeVGVSpHK1BnqtGDtGAumyZyGo07KkRR-jXUnbZZWDQHcSVPZs1skgJeip5XsM3a8kfQd6A0PXozoNFp6xer71uGYZ9RsNKHiUMVi84',
+            product_url: '',
+        },
+    ]
+}
